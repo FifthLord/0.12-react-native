18c7c0f13e5ce2c9146efa6031b9e5b7f2d43954
FifthLord <nborysenko777@gmail.com>
2023-03-09 21:23:56 +0200

    added some attributes

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
    return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
          <Text>Open up App.js to start working on your app!</Text>
+         <Button title={'Натисни'} />
          <StatusBar style="auto" />
-      </View>
+      </SafeAreaView>
    );
 }
 
