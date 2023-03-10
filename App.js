f1d4cabdf7117ee84c341c941ba486707eb9c5aa
FifthLord <nborysenko777@gmail.com>
2023-03-10 16:05:33 +0200

    added fonts and AppLoading

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Button, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { gStyle } from './styles/style';
+import * as Font from 'expo-font'
+import { useState } from 'react';
+import AppLoading from 'expo-app-loading';
+
+
+const fonts = () => Font.loadAsync({
+   'mt-regular': require('./assets/fonts/Montserrat-Regular.ttf'),
+   'mt-bold': require('./assets/fonts/Montserrat-Bold.ttf')
+})
 
 export default function App() {
-   return (
-      <SafeAreaView style={styles.container}>
-         <Text>Open up App.js to start working on your app!</Text>
-         <Button title={'Натисни'} />
-         <StatusBar style="auto" />
-      </SafeAreaView>
-   );
+   const [font, setFont] = useState(false)
+
+   if (font) {
+      return (
+         <View style={gStyle.main}>
+            <Text style={gStyle.title}>Open up App.js to start working on your app!</Text>
+         </View>
+      );
+   } else {
+      return (
+         <AppLoading startAsync={fonts} onFinish={() => setFont(true)} onError={err => console.warn(err)} />
+      )
+   }
 }
 
 const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-   },
+
 });
