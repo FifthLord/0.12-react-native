6774e35ac6e1b517b40a0faeeed2f513dc6acc5d
FifthLord <nborysenko777@gmail.com>
2023-03-11 21:43:17 +0200

    created navigation file

---
+++ navigate.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Main from './components/Main';
+import Contacts from './components/Contacts';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export default function Navigate() {
+   return <NavigationContainer>
+      <Stack.Navigator>
+         <Stack.Screen
+            name='Main'
+            component={Main}
+            options={{ title: "Головна" }}
+         />
+         <Stack.Screen
+            name='Contacts'
+            component={Contacts}
+            options={{ title: "Контакти" }}
+         />
+      </Stack.Navigator>
+   </NavigationContainer>
+}
