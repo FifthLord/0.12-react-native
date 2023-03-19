f60efaa886c9d1973917d879c8f793de25a960e6
FifthLord <nborysenko777@gmail.com>
2023-03-19 13:34:03 +0200

    added input  in form

--- components/Form.js
+++ components/Form.js
@@ -6,10 +6,38 @@ import { Formik } from 'formik';
 export default function Form() {
    return (
       <View style={gStyle.main}>
-         <Formik initialValues={{ name: '', anons: '', full: '', img: '' }} onSubmit={(values) => {
-            console.log(values);
-         }}>
-
+         <Formik
+            initialValues={{ name: '', anons: '', full: '', img: '' }}
+            onSubmit={(values) => {
+               console.log(values);
+            }}>
+            {(props) => (
+               <View>
+                  <TextInput
+                     value={props.values.name}
+                     placeholder='Введіть назву'
+                     onChangeText={props.handleChange('name')}
+                  />
+                  <TextInput
+                     multiline
+                     value={props.values.anons}
+                     placeholder='Введіть анонс'
+                     onChangeText={props.handleChange('anons')}
+                  />
+                  <TextInput
+                     multiline
+                     value={props.values.full}
+                     placeholder='Введіть статтю'
+                     onChangeText={props.handleChange('full')}
+                  />
+                  <TextInput
+                     value={props.values.img}
+                     placeholder='Додайте зображення'
+                     onChangeText={props.handleChange('img')}
+                  />
+                  <Button title='Додати' onPress={props.handleSubmit} />
+               </View>
+            )}
          </Formik>
       </View>
    );
